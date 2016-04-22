336cf07893861624be1be80320f8c0c97e064f83
fatcloud <victor19871226@hotmail.com>
2016-04-23 05:51:23 +0800

    write some conceptual comments

---
+++ lalay.py
@@ -0,0 +1,4 @@
+# collect a price list
+# list existing class tags
+# Specify a set of tags, a specific price, and a limt of item numbers
+# search for a collection of items whose price adds up to the specified value
