e65cb94e96ab72ec1e500e4821c2149ccb54cefb
fatcloud <victor19871226@hotmail.com>
2016-10-13 02:59:32 +0800

    add the cwtex thing

--- lalay.py
+++
@@ -1,4 +0,0 @@
-# collect a price list
-# list existing class tags
-# Specify a set of tags, a specific price, and a limt of item numbers
-# search for a collection of items whose price adds up to the specified value
